96b6bd3e7f01e23d88e95401f7cb305e1e681f3a
LarysaTymoshenko <luda76@ukr.net>
2022-01-29 22:24:39 +0200

    contactReducer

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { rootStore } from './redux/store.js';
-import { PersistGate } from 'redux-persist/integration/react';
+import { store } from './redux/store.js';
+// import { PersistGate } from 'redux-persist/integration/react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './components/App/App.jsx';
@@ -9,12 +9,10 @@ import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={rootStore.store}>
-      <PersistGate loading={null} persistor={rootStore.persistor}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </PersistGate>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,62 +1,22 @@
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/dist/query';
 import logger from 'redux-logger';
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-import { contactsReducer } from './contact/contact-reducer.js';
-import { contactApi } from './contact/contactSlice';
-
-const rootReducer = persistReducer(persistConfig, contactsReducer);
+import { contactsReducer } from './contact/contact-reducer';
+// import { filterReducer } from './Filter/filter-reducer';
 
 const middleware = getDefaultMiddleware => [
-  ...getDefaultMiddleware({
-    serializableCheck: {
-      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-    },
-  }),
+  ...getDefaultMiddleware(),
+  contactsReducer.middleware,
   logger,
 ];
 
-const store = configureStore({
+export const store = configureStore({
   reducer: {
-    rootReducer,
-    [contactApi.reducerPath]: contactApi.reducer,
+    [contactsReducer.reducerPath]: contactsReducer.reducer,
+    // filter: filterReducer,
   },
-
   middleware,
   devTools: process.env.NODE_ENV === 'development',
 });
 
-const persistor = persistStore(store);
-
-export const rootStore = { store, persistor };
-
-// import { configureStore } from '@reduxjs/toolkit';
-// import { setupListeners } from '@reduxjs/toolkit/dist/query';
-// import logger from 'redux-logger';
-// import { contactsReducer } from './Contacts/contacts-reducer';
-// import { filterReducer } from './Filter/filter-reducer';
-
-// const middleware = getDefaultMiddleware => [
-//   ...getDefaultMiddleware(),
-//   contactsReducer.middleware,
-//   logger,
-// ];
-
-// export const store = configureStore({
-//   reducer: {
-//     [contactsReducer.reducerPath]: contactsReducer.reducer,
-//     filter: filterReducer,
-//   },
-//   middleware,
-//   devTools: process.env.NODE_ENV === 'development',
-// });
-
-// setupListeners(store.dispatch);
+setupListeners(store.dispatch);
